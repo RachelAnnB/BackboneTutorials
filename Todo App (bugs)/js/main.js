7554ec3b98e6613c37399308bd4f731d4b8ee439
Rachel Ann Broering <rachelannbroering@gmail.com>
2015-08-12 21:03:18 -0400

    Deleted some folders, and edited the basic setup files

--- Todo App (bugs)/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    //HERE IS OUR COLLECTION
-    var todoList = new TodoList();
-    todoList.fetch();
-    
-    //HERE IS OUR VIEW
-    var todoListView = new TodoListView({ model: todo });
-    $("body").append(todoListView.render().$el);
-});
